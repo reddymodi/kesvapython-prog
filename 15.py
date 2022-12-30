a20c0b8ed6bfde4a995c4a2710866e9acbf08d2b
reddymodi <114898950+reddymodi@users.noreply.github.com>
2022-12-30 11:43:42 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,16 +1,14 @@
-def toggle(s1,s2):
-    l1=len(s1)
-    l2=len(s2)
-    if l1 == l2:
-        for i in s1:
-            if i not in s2:
-                print("not a angram")
-            else:
-                print("its anagram")
-    else:
-        print("its not a anagram strings")
-
 s1=input()
 s2=input()
-toggle()
+i=0
+if len(s1) == len(s2):
+    k=s1[i]
+    if k in s2:
+        print("its a anagram")
+        i += 1
+    else:
+        print("is not anagram")
+else:
+    print("its not a angram string")
+        
     
