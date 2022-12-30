e68bc127c711eb7a9743136e6b3d78e68b140af9
reddymodi <114898950+reddymodi@users.noreply.github.com>
2022-12-30 11:53:18 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,12 +1,13 @@
-str1 = "aaabbbcccdddd"
-k = {}
-for i in str1:
-    if i in k.keys():
-        k[i] += 1
-    else:
-        k[i] = 1
+def cap(n):
+    l = len(n)
+    i = 0
 
-res = ""
-for key,val in k.items():
-    res += key+str(val)
-print(res)
+    while i<l-1:
+        count=1
+        while(i<l-1and (n[i]==n[i+1])):
+            count += 1 
+            i += 1
+        i += 1 
+        print(n[i-1]+str(count),end="")
+n = input("enter the charecters: ")
+cap(n)
